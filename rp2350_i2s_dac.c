45087eb4d2b9fce95104eca3b684523df57569b6
Richard Campbell <rlcamp@users.noreply.github.com>
2026-06-04 22:13:45 -0700

    use xorshift32 for dither

--- rp2350_i2s_dac.c
+++ rp2350_i2s_dac.c
@@ -35,22 +35,21 @@ static float cmagsquaredf(const float complex x) {
     return crealf(x) * crealf(x) + cimagf(x) * cimagf(x);
 }
 
-static uint64_t xorshift64star(void) {
-    /* marsaglia et al., yields 64 bits, most significant are most random */
-    static uint64_t x = 1; /* must be nonzero */
-    x ^= x >> 12;
-    x ^= x << 25;
-    x ^= x >> 27;
-    return x * 0x2545F4914F6CDD1DULL;
+static uint32_t xorshift32(void) {
+    static uint32_t x = 1;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    return x;
 }
 
 static float frand_minus_frand(void) {
-    /* generate 64 random bits, of which we will use the most significant 46, in two groups of 23 */
-    const uint64_t bits = xorshift64star();
+    /* generate 32 random bits, we will fill the highest 16 bits of each 23-bit mantissa */
+    const uint32_t bits = xorshift32();
 
     /* generate two random numbers each uniformly distributed on [1.0f, 2.0f) */
-    const union { uint32_t u; float f; } x = { .u = 0x3F800000U | ((bits >> 41) & 0x7FFFFFU) };
-    const union { uint32_t u; float f; } y = { .u = 0x3F800000U | ((bits >> 18) & 0x7FFFFFU) };
+    const union { uint32_t u; float f; } x = { .u = 0x3F800000U | (((bits >> 16) & 0xFFFF) << 7) };
+    const union { uint32_t u; float f; } y = { .u = 0x3F800000U | ((bits & 0xFFFF) << 7) };
 
     /* and subtract them, yielding a triangular distribution on (-1.0f, +1.0f) */
     return x.f - y.f;
